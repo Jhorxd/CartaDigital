dfe6a58277d1d68783ada44953b48de16a2c580a
Jhorxd <jhordanroly22@gmail.com>
2026-04-10 22:19:33 -0500

    Logss

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,12 +11,22 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Traits\BelongsToTenant;
 
-#[Fillable(['name', 'email', 'password', 'tenant_id'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, BelongsToTenant;
+    use HasFactory, Notifiable; //, BelongsToTenant;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'tenant_id',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
